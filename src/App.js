a9d4519b357fc87ffa95fc672e99859e76652fc3
Samair <samair.zia@tenpearls.com>
2023-03-30 00:57:30 +0500

    Suing transition component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -23,21 +24,48 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <button className="Button" onClick={ () => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
-        {this.state.showBlock ? (
-          <div
-            style={{
-              backgroundColor: "red",
-              width: 100,
-              height: 100,
-              margin: "auto",
-            }}
-          ></div>
-        ) : null}
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        {/* in property is on which state/ condition we wana perform transition show / hide  */}
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter // adding these porps in porder to remove or add the element from dom
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+        >
+          {(
+            state // here state is the state in which the element is entered exited etc
+          ) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
         <br />
 
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
+
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
