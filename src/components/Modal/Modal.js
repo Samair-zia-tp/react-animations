14fa7b01c89989c1dcec09aef26a2c090702c951
Samair <samair.zia@tenpearls.com>
2023-03-30 02:07:17 +0500

    Using transitiongroup

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,37 +1,39 @@
 import React from "react";
-import { Transition } from "react-transition-group";
+import { CSSTransition, Transition } from "react-transition-group";
 
 import "./Modal.css";
 
-// we can set different timings for entering and exiting the animation 
+// we can set different timings for entering and exiting the animation
 
 const animationTiming = {
   enter: 400,
-  exit: 1000
-}
+  exit: 1000,
+};
 
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      //one way of adding trunck of classes
+      // classNames='fade-slide' // clasname is up to us, it itslef add these classes as per the state of the tranisition - fade-slide-enter, fade-slide-enter-active, fade-slide-exit, fade-slide-exit-active
+      //second way our own custom classes for every state
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed'
       }}
-    </Transition>
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
